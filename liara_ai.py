ee5f9a0b05f5ff78d9fed0c0923294699e080806
dokja124 <malandaniel250@gmail.com>
2026-06-16 20:44:25 +0000

    Update liara_ai.py

--- liara_ai.py
+++ liara_ai.py
@@ -8,7 +8,7 @@ LIARA_BASE_URL = "https://ai.liara.ir/api/6940fe21363673561d9a83cb/v1"
 LIARA_API_KEY = os.getenv("LIARA_API_KEY")
 LIARA_MODEL = "google/gemini-2.0-flash-001"
 
-client = OpenAI(base_url=LIARA_BASE_URL, api_key=org-CSTLuQsdmDWngiACu6s3ZzhB)
+client = OpenAI(base_url=LIARA_BASE_URL, api_key=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)
 
 def generate_with_liara(full_prompt):
     try:    
